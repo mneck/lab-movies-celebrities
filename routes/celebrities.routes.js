dfa4496fbfc86a9fbf8f2338e4aeab2cf522dac2
Matt N <mneckelmann@gmail.com>
2023-05-01 20:27:43 +0200

    update

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -19,20 +19,6 @@ router.get("/celebrities", (req, res, next) => {
     .catch((err) => next(err));
 });
 
-// router.post("/celebrities/create", (req, res, next) => {
-//   // Add celebrity
-//   Celebrity.find()
-//     .then((celebrityFromDB) => {
-//       console.log(celebrityFromDB);
-//       res.render("celebrities/new-celebrity", {
-//         name,
-//         occupation,
-//         catchPhrase,
-//       });
-//     })
-//     .catch((err) => next(err));
-// });
-
 router.post("/celebrities", (req, res, next) => {
   console.log(req.body);
   const { name, occupation, catchPhrase } = req.body;
@@ -46,31 +32,3 @@ router.post("/celebrities", (req, res, next) => {
 });
 
 module.exports = router;
-
-/*
-Steps we will follow in this iteration:
-1. In the routes file (routes/celebrities.routes.js) create the 
-following GET route: /celebrities/create
-2. In that route we have to render the celebrities/new-celebrity view
-3. In that view file:
-Add an <h2> for the page's heading.
-Add a <form> tag that makes a POST request to /celebrities/create.
-Add <input> tags inside the form so the user can fill in values for
- each attribute of the celebrity. Make an input for name, occupation,
-  and catchPhrase
-Add a <button> tag in the form so the user can submit the form once 
-they are done filling it out.
-4. Create the /celebrities/create POST route in 
-routes/celebrities.routes.js.
-5. In that route we have to create an instance of the Celebrity model 
-(don't forget, we should get all the info from the form through 
-req.body)
-If there is an error, render the celebrities/new-celebrity view so
- the user can try again and
-If there is no error, redirect to the page with the list of 
-celebrities. This route will be created in the next iteration of 
-/celebrities
-6. In the views/index.hbs view file:
-Add a link that goes to the page you just created with the form 
-to create a new celebrity.
-*/
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,37 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
 
 // all your routes here
+
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+
+router.get("/movies/create", (req, res, next) => {
+  // Create celebrity
+  Celebrity.find()
+    .then((celebritiesFromDB) => {
+      res.render("movies/new-movie", { celebrities: celebritiesFromDB });
+    })
+    .catch((err) => next(err));
+});
+
+router.post("/movies", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  Movie.create({ title, genre, plot, cast })
+    .then((createdMovie) => {
+      console.log(createdMovie);
+      res.redirect("/movies");
+    })
+    .catch((err) => err.redirect("/movies/create"));
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((moviesFromDB) => {
+      console.log(moviesFromDB);
+      res.render("movies/movies", { movie: moviesFromDB });
+    })
+    .catch((err) => next(err));
+});
 
 module.exports = router;
